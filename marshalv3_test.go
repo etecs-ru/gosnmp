94b9aee3ae91a2bad8e9206eec6495fc522a7406
lilinzhe <slayercat.subscription@gmail.com>
2020-02-11 15:34:14 +0800

    marshalv3_test.go: TestUnmarshalMsgV3NotEncrypted

--- marshalv3_test.go
+++ marshalv3_test.go
@@ -32,6 +32,30 @@ func TestUnmarshalMsgV3NotEncrypted(t *testing.T) {
 	}
 }
 
+
+func TestUnmarshalMsgV3Encrypted(t *testing.T) {
+	Default.Logger = log.New(ioutil.Discard, "", 0)
+
+	vhandle := GoSNMP{}
+	vhandle.SecurityParameters=&UsmSecurityParameters{
+		UserName:                 "testuser",
+		AuthenticationProtocol:   MD5,
+		PrivacyProtocol:          DES,
+		AuthenticationPassphrase: "testauth",
+		PrivacyPassphrase:        "testpriv",
+		Logger:                   Default.Logger,
+	}
+	vhandle.Logger = Default.Logger
+	testBytes := v3Encrypted()
+	res, err := vhandle.SnmpDecodePacket(testBytes)
+	if err != nil {
+		t.Errorf("#v3UnknownPasswordEncryptedPDU: SnmpDecodePacket() meet err %v", err)
+	}
+	if res.SecurityParameters.(*UsmSecurityParameters).UserName != "testuser" {
+		t.Errorf("#v3UnknownPasswordEncryptedPDU: SnmpDecodePacket() err Username: %v [expect simulator]", res.SecurityParameters.(*UsmSecurityParameters).UserName)
+	}
+}
+
 // Simple Network Management Protocol
 // msgVersion: snmpv3 (3)
 // msgGlobalData
@@ -135,5 +159,69 @@ func v3NotEncrypted() []byte {
 		0x12, 0x30, 0x10, 0x06, 0x0c, 0x2b, 0x06, 0x01,
 		0x02, 0x01, 0x2b, 0x0e, 0x01, 0x01, 0x06, 0x01,
 		0x05, 0x05, 0x00,
+	}
+}
+
+// Simple Network Management Protocol
+//     msgVersion: snmpv3 (3)
+//     msgGlobalData
+//         msgID: 1246299848
+//         msgMaxSize: 65507
+//         msgFlags: 07
+//             .... .1.. = Reportable: Set
+//             .... ..1. = Encrypted: Set
+//             .... ...1 = Authenticated: Set
+//         msgSecurityModel: USM (3)
+//     msgAuthoritativeEngineID: 80004fb8054445534b544f502d4a3732533245343ab63bc8
+//         1... .... = Engine ID Conformance: RFC3411 (SNMPv3)
+//         Engine Enterprise ID: pysnmp (20408)
+//         Engine ID Format: Octets, administratively assigned (5)
+//         Engine ID Data: 4445534b544f502d4a3732533245343ab63bc8
+//     msgAuthoritativeEngineBoots: 1
+//     msgAuthoritativeEngineTime: 514241
+//     msgUserName: testuser
+//     msgAuthenticationParameters: c72b2bec27bdf6bc3e8ea66a
+//     msgPrivacyParameters: 00000001b0bd8ffd
+//     msgData: encryptedPDU (1)
+//         encryptedPDU: e0f055df57179446c914f4201f7892627738c87d52584e83…
+//             Decrypted ScopedPDU: 3039041880004fb8054445534b544f502d4a373253324534…
+//                 contextEngineID: 80004fb8054445534b544f502d4a3732533245343ab63bc8
+//                     1... .... = Engine ID Conformance: RFC3411 (SNMPv3)
+//                     Engine Enterprise ID: pysnmp (20408)
+//                     Engine ID Format: Octets, administratively assigned (5)
+//                     Engine ID Data: 4445534b544f502d4a3732533245343ab63bc8
+//                 contextName: public
+//                 data: get-next-request (1)
+//                     get-next-request
+//                         request-id: 1224110830
+//                         error-status: noError (0)
+//                         error-index: 0
+//                         variable-bindings: 1 item
+//                             itu-t.1 (0.1): Value (Null)
+//                                 Object Name: 0.1 (itu-t.1)
+//                                 Value (Null)
+func v3Encrypted() []byte{
+	return []byte{
+		0x30, 0x81, 0xa0, 0x02, 0x01, 0x03, 0x30, 0x11,
+  0x02, 0x04, 0x4a, 0x49, 0x06, 0xc8, 0x02, 0x03,
+  0x00, 0xff, 0xe3, 0x04, 0x01, 0x07, 0x02, 0x01,
+  0x03, 0x04, 0x46, 0x30, 0x44, 0x04, 0x18, 0x80,
+  0x00, 0x4f, 0xb8, 0x05, 0x44, 0x45, 0x53, 0x4b,
+  0x54, 0x4f, 0x50, 0x2d, 0x4a, 0x37, 0x32, 0x53,
+  0x32, 0x45, 0x34, 0x3a, 0xb6, 0x3b, 0xc8, 0x02,
+  0x01, 0x01, 0x02, 0x03, 0x07, 0xd8, 0xc1, 0x04,
+  0x08, 0x74, 0x65, 0x73, 0x74, 0x75, 0x73, 0x65,
+  0x72, 0x04, 0x0c, 0xc7, 0x2b, 0x2b, 0xec, 0x27,
+  0xbd, 0xf6, 0xbc, 0x3e, 0x8e, 0xa6, 0x6a, 0x04,
+  0x08, 0x00, 0x00, 0x00, 0x01, 0xb0, 0xbd, 0x8f,
+  0xfd, 0x04, 0x40, 0xe0, 0xf0, 0x55, 0xdf, 0x57,
+  0x17, 0x94, 0x46, 0xc9, 0x14, 0xf4, 0x20, 0x1f,
+  0x78, 0x92, 0x62, 0x77, 0x38, 0xc8, 0x7d, 0x52,
+  0x58, 0x4e, 0x83, 0xea, 0x35, 0x02, 0x0d, 0x6a,
+  0x25, 0xc9, 0x74, 0xa6, 0xbd, 0x5f, 0xb7, 0x5f,
+  0x6e, 0x3f, 0xee, 0x3c, 0xc6, 0x8b, 0x14, 0x98,
+  0xe7, 0x18, 0x85, 0x2e, 0x5b, 0xe4, 0x95, 0x8a,
+  0x1c, 0xa3, 0x7f, 0x77, 0x50, 0xfc, 0xe4, 0x04,
+  0x3d, 0x93, 0x9e,
 	}
 }
