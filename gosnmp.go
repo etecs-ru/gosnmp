29cae7052339b76d49d95baccd21e6f4c546c6c7
lilinzhe <slayercat.subscription@gmail.com>
2020-02-16 10:12:51 +0800

    Set: remove barrier for all pdutype

--- gosnmp.go
+++ gosnmp.go
@@ -357,13 +357,7 @@ func (x *GoSNMP) Get(oids []string) (result *SnmpPacket, err error) {
 // Set sends an SNMP SET request
 func (x *GoSNMP) Set(pdus []SnmpPDU) (result *SnmpPacket, err error) {
 	var packetOut *SnmpPacket
-	switch pdus[0].Type {
-	// TODO test Gauge32
-	case Integer, OctetString, Gauge32, IPAddress:
-		packetOut = x.mkSnmpPacket(SetRequest, pdus, 0, 0)
-	default:
-		return nil, fmt.Errorf("ERR:gosnmp currently only supports SNMP SETs for Integers, IPAddress and OctetStrings")
-	}
+	packetOut = x.mkSnmpPacket(SetRequest, pdus, 0, 0)
 	return x.send(packetOut, true)
 }
 
